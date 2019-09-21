425e6a3de2738be3666db01eb07343210bd77038
Zachary Schulz <zjschulz@gmail.com>
2019-09-21 00:01:32 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,8 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    if yield(collection[i])
     return collection[i]
+    if yield(collection[i])
+    
     end
     i = i + 1
   end
